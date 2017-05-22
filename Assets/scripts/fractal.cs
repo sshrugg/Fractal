0689e312ce4b5fff564409dee1789a9846da7d46
Daniel Aber <sshrugg@gmail.com>
2017-05-21 22:00:30 -0700

    Showing Something
    -Add Mesh and Material public variables to "fractal.cs"
    -Editor: fractal.cs ->Mesh = Cube
    -Editor: fractal.cs -> Material = Default Material
    -Add these properties to object "Fractal" on Start()

--- Assets/scripts/fractal.cs
+++ Assets/scripts/fractal.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class fractal : MonoBehaviour {
 
+    public Mesh mesh;
+    public Material material;
+
 	// Use this for initialization
 	void Start () {
-		
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+
 	}
 	
 	// Update is called once per frame
