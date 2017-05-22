f6a58a8ac1b2318e7d3d9240fc1084ad442aae84
Daniel Aber <sshrugg@gmail.com>
2017-05-21 22:29:19 -0700

    Making Children
    -if loop recursively adds GameObjects and attaches fractal.cs to the object.
    -maxChildren sets number of nested GameObjects
    -depth variable keeps track of how many GameObjects have been created

--- Assets/scripts/fractal.cs
+++ Assets/scripts/fractal.cs
@@ -6,16 +6,33 @@ public class fractal : MonoBehaviour {
 
     public Mesh mesh;
     public Material material;
+    [Range(1, 1000)]
+    public int maxChildren = 5;
+    private int depth=1;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth <= maxChildren)
+        {
+            new GameObject("Fractal Child" + depth).AddComponent<fractal>().Initialize(this);
+        }
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Initialize(fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxChildren = parent.maxChildren;
+        depth = parent.depth + 1;
+        transform.parent = parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
